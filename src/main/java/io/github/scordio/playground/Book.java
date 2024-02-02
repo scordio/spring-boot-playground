0072ac6b251712e6fdd895ca67dd55c0d61efaa5
Stefano Cordio <stefano_cordio@epam.com>
2024-02-02 15:03:37 +0100

    Add Spring Data JPA reproducer for Spring Boot 2.7

---
+++ src/main/java/io/github/scordio/playground/Book.java
@@ -0,0 +1,55 @@
+package io.github.scordio.playground;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = true)  // left join
+//	@ManyToOne(fetch = FetchType.LAZY, optional = false) // join
+	private Author author;
+
+	public Book() {
+	}
+
+	public Book(String name, Author author) {
+		this.name = name;
+		this.author = author;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Author getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+
+}
---
+++ src/test/java/io/github/scordio/playground/BookRepositoryTest.java
@@ -0,0 +1,33 @@
+package io.github.scordio.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+@DataJpaTest(showSql = false) // use P6Spy
+class BookRepositoryTest {
+
+	@Autowired
+	private BookRepository underTest;
+
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@Test
+	void findAllByAuthorId() {
+		// insert into author (name, id) values ('Homer', 1);
+		Author homer = authorRepository.save(new Author("Homer"));
+
+		// insert into book (author_id, name, id) values (1, 'Iliad', 2);
+		underTest.save(new Book("Iliad", homer));
+
+		// insert into book (author_id, name, id) values (1, 'Odyssey', 3);
+		underTest.save(new Book("Odyssey", homer));
+
+		// select book0_.id as id1_1_, book0_.author_id as author_i3_1_, book0_.name as name2_1_ from book book0_ left outer join author author1_ on book0_.author_id=author1_.id where author1_.id=1;
+		List<Book> books = underTest.findAllByAuthorId(homer.getId());
+	}
+
+}
