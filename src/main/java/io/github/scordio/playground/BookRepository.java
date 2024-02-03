a8bd5fa6ee8edd228a041b662b795c78f528fc71
Stefano Cordio <stefano_cordio@epam.com>
2024-02-03 13:52:26 +0100

    Add JPQL variant

--- src/main/java/io/github/scordio/playground/BookRepository.java
+++ src/main/java/io/github/scordio/playground/BookRepository.java
@@ -1,6 +1,8 @@
 package io.github.scordio.playground;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,5 +11,8 @@ interface BookRepository extends Repository<Book, Long> {
 	Book save(Book book);
 
 	List<Book> findAllByAuthorId(Long authorId);
+
+	@Query("SELECT b FROM Book b WHERE b.author.id = :authorId")
+	List<Book> findAllByAuthorIdWithJPQL(@Param("authorId") Long authorId);
 
 }
--- src/test/java/io/github/scordio/playground/BookRepositoryTest.java
+++ src/test/java/io/github/scordio/playground/BookRepositoryTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.List;
-
 @DataJpaTest(showSql = false) // use P6Spy
 class BookRepositoryTest {
 
@@ -27,7 +25,10 @@ class BookRepositoryTest {
 		underTest.save(new Book("Odyssey", homer));
 
 		// select b1_0.id,b1_0.author_id,b1_0.name from book b1_0 left join author a1_0 on a1_0.id=b1_0.author_id where a1_0.id=1;
-		List<Book> books = underTest.findAllByAuthorId(homer.getId());
+		underTest.findAllByAuthorId(homer.getId());
+
+		// select b1_0.id,b1_0.author_id,b1_0.name from book b1_0 where b1_0.author_id=1;
+		underTest.findAllByAuthorIdWithJPQL(homer.getId());
 	}
 
 }
